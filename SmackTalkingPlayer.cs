2ae1fdb87cf4843df11feb58cabdd01850548f07
soyuz43 <kebekad673@proton.me>
2025-05-17 18:07:54 -0500

    Add SmackTalkingPlayer with taunt functionality

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,8 +1,17 @@
-namespace ShootingDice;
-// TODO: Complete this class
+using System;
+using ShootingDice;
 
-// A Player who shouts a taunt every time they roll dice
-public class SmackTalkingPlayer
+namespace ShootingDice
 {
-    public string Taunt { get; }
+    // A Player who shouts a taunt every time they roll dice
+    public class SmackTalkingPlayer : Player
+    {
+        public string Taunt { get; set; } = "You're going down!";
+
+        public override void Play(Player other)
+        {
+            Console.WriteLine($"{Name} says: {Taunt}");
+            base.Play(other);
+        }
+    }
 }
