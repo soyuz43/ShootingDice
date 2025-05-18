b9a5d554b21fc4712adf7818c80b95128d4dc012
soyuz43 <kebekad673@proton.me>
2025-05-18 08:38:18 -0500

    Add OneHigherPlayer class

--- OneHigherPlayer.cs
+++ OneHigherPlayer.cs
@@ -1,8 +1,33 @@
 namespace ShootingDice;
-// TODO: Complete this class
 
-// Override the Play method to make a Player who always roles one higher than the other player
-public class OneHigherPlayer
+// A player who always rolls one higher than the other player
+public class OneHigherPlayer : Player
 {
+    public override void Play(Player other)
+    {
+        int otherRoll = other.Roll();
+        int myRoll = otherRoll + 1;
 
+        // Clamp roll if it exceeds dice size
+        if (myRoll > DiceSize)
+        {
+            myRoll = DiceSize;
+        }
+
+        Console.WriteLine($"{Name} rolls a {myRoll} (cheated)");
+        Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+
+        if (myRoll > otherRoll)
+        {
+            Console.WriteLine($"{Name} Wins!");
+        }
+        else if (myRoll < otherRoll)
+        {
+            Console.WriteLine($"{other.Name} Wins!");
+        }
+        else
+        {
+            Console.WriteLine("It's a tie");
+        }
+    }
 }
