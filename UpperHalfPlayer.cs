5a164c858d0ec50dc5b9a744d6c46cf09c2644b1
soyuz43 <kebekad673@proton.me>
2025-05-19 10:35:50 -0500

    Add UpperHalfPlayer: always rolls in upper range

--- UpperHalfPlayer.cs
+++ UpperHalfPlayer.cs
@@ -1,8 +1,11 @@
 namespace ShootingDice;
-// TODO: Complete this class
 
-// A Player whose role will always be in the upper half of their possible rolls
-public class UpperHalfPlayer
+// A Player whose roll will always be in the upper half of their possible rolls
+public class UpperHalfPlayer : Player
 {
-
+    public override int Roll()
+    {
+        int min = (DiceSize / 2) + 1;
+        return new Random().Next(min, DiceSize + 1);
+    }
 }
